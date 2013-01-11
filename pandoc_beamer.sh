4210b461767369036c8120a365f57fd329536eb2
James E. Marca <jmarca@translab.its.uci.edu>
2013-01-10 16:17:00 -0800

    this time a charm?

--- pandoc_beamer.sh
+++ pandoc_beamer.sh
@@ -1,1 +1,1 @@
-pandoc -t beamer --latex-engine=xelatex  present.md -V theme:Singapore --standalone -o present.pdf 
+pandoc -t beamer --latex-engine=xelatex  present.md -V theme:Singapore -o present.pdf 
